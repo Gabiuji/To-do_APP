98ea8e031a4aa6b8bc548562e813040c6d567053
Gabiuji <ogabrieliuji@gmail.com>
2025-05-24 19:20:11 -0400

    Adição do Model

--- src/main/java/com/example/to_do/model/Tarefa.java
+++ src/main/java/com/example/to_do/model/Tarefa.java
@@ -1,5 +1,29 @@
 package com.example.to_do.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Tarefa {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefa() {}
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+    public Long getId() { return id; }
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+    public boolean isConcluida() { return concluida; }
+    public void setConcluida(boolean concluida) { this.concluida = concluida; }
 }
